265eec126e55a57f6bad28e62c599c118d4af30e
charlesthemonkey <csinkler@wpi.edu>
2016-11-28 18:19:30 -0500

    Started GUI for player level select with scrollable level panels.

---
+++ LetterCraze/src/playerGUI/LevelSelectMenu.java
@@ -0,0 +1,112 @@
+package playerGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+
+public class LevelSelectMenu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LevelSelectMenu window = new LevelSelectMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LevelSelectMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
+				);
+
+		JScrollPane puzzleScrollPane = new JScrollPane();
+
+		JScrollPane lightningScrollPane = new JScrollPane();
+
+		JScrollPane themeScrollPane = new JScrollPane();
+
+		JLabel lblPuzzleLevels = new JLabel("Puzzle Levels");
+
+		JLabel lblLightningLevels = new JLabel("Lightning Levels");
+
+		JLabel lblThemeLevels = new JLabel("Theme Levels");
+
+		JButton btnBack = new JButton("Back");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+				gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+						.addGap(20)
+						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnBack)
+								.addComponent(lblPuzzleLevels)
+								.addComponent(lblLightningLevels)
+								.addComponent(lblThemeLevels)
+								.addComponent(themeScrollPane, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)
+								.addComponent(lightningScrollPane, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)
+								.addComponent(puzzleScrollPane, GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE))
+						.addGap(20))
+				);
+		gl_panel.setVerticalGroup(
+				gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(btnBack)
+						.addGap(5)
+						.addComponent(lblPuzzleLevels)
+						.addGap(20)
+						.addComponent(puzzleScrollPane, GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE)
+						.addGap(20)
+						.addComponent(lblLightningLevels)
+						.addGap(20)
+						.addComponent(lightningScrollPane, GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE)
+						.addGap(20)
+						.addComponent(lblThemeLevels)
+						.addGap(20)
+						.addComponent(themeScrollPane, GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
+						.addGap(20))
+				);
+		panel.setLayout(gl_panel);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}

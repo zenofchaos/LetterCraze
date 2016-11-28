1ab1eab2b7d2237566c994e6b7fe50c2507c7b17
kcorton <kcorton@wpi.edu>
2016-11-28 12:10:32 -0500

    BuilderBoard class added

---
+++ LetterCraze/src/BuilderFiles/BuilderBoard.java
@@ -0,0 +1,32 @@
+package BuilderFiles;
+
+import BuilderFiles.BuilderSquare;
+
+public class BuilderBoard {
+
+BuilderSquare[][] squares;
+	
+	BuilderBoard(BuilderSquare[][] squares){
+		if((squares.length == 6) && squares[0].length == 6){
+			this.squares = squares;
+		}
+		else{
+			System.err.println ("incorrect square array passed to build board");
+		}
+		
+	}
+	
+	BuilderSquare[][] getSquares(){
+		return this.squares;
+	}
+	
+	boolean setSquares(BuilderSquare[][] squares){
+		if((squares.length == 6) && squares[0].length == 6){
+			this.squares = squares;
+			return true;
+		}
+		else{ 
+			return false;
+		}
+	}
+}

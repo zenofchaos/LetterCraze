48981fcd7f733eeaf4ba80feff6c76c4f6421e0b
Kevin <Kevin@130.215.222.75>
2016-11-29 18:39:21 -0500

    Added the beginnings of PlayerLetterCraze and PlayerSelectLevelController.

---
+++ LetterCraze/src/playerFiles/PlayerLetterCraze.java
@@ -0,0 +1,34 @@
+package playerFiles;
+
+public class PlayerLetterCraze {
+
+	PlayerModel model;
+	PlayerLevel activeLevel;
+	
+	public PlayerLetterCraze(){
+		initializeModel();
+		initializeView();
+		initializeControllers();
+	}
+
+	public void setActiveLevel(PlayerLevel toSet){
+		this.activeLevel = toSet;
+	}
+	
+	private void initializeModel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initializeView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initializeControllers() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}

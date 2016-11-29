6460180940bb4625235361534fd5c5107697c020
charlesthemonkey <csinkler@wpi.edu>
2016-11-29 13:15:45 -0500

    Updated to be more dynamic (resizing works better)

--- LetterCraze/src/playerGUI/PlayerMainMenu.java
+++ LetterCraze/src/playerGUI/PlayerMainMenu.java
@@ -11,6 +11,8 @@ import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class PlayerMainMenu extends JFrame {
 
@@ -37,24 +39,35 @@ public class PlayerMainMenu extends JFrame {
 	 */
 	public PlayerMainMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 640, 480);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setForeground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Letter Craze");
+		JLabel lblNewLabel = new JLabel("Letter Craze Player");
 		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 20));
-		lblNewLabel.setBounds(12, 28, 426, 59);
-		contentPane.add(lblNewLabel);
 		
 		JButton btnLevels = new JButton("Levels");
 		btnLevels.setFont(new Font("Dialog", Font.BOLD, 15));
-		btnLevels.setBounds(164, 116, 117, 25);
-		contentPane.add(btnLevels);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.CENTER)
+				.addComponent(lblNewLabel, Alignment.CENTER, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+				.addComponent(btnLevels, Alignment.CENTER, GroupLayout.PREFERRED_SIZE, 0, 200)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(40, Short.MAX_VALUE)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 59, GroupLayout.PREFERRED_SIZE)
+					.addGap(40)
+					.addComponent(btnLevels, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(200, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 }

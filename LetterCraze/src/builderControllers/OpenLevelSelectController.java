9385fd07a6036a5e5ccb7b689d5a365daf422811
Linda <Javario@130.215.250.96>
2016-11-29 18:26:51 -0500

    More progress on OpenLevelSelectControllers
    Now open every Lightning level file, extract data, and... nothing. Next
    step is to generate levels and add them to the menu.

--- LetterCraze/src/builderControllers/OpenLevelSelectController.java
+++ LetterCraze/src/builderControllers/OpenLevelSelectController.java
@@ -2,16 +2,18 @@ package builderControllers;
 
 import java.util.Scanner;
 
+import builderFiles.*;
+
 public class OpenLevelSelectController {
-	
-	//Store the number of available levels in the directory.
+
+	// Store the number of available levels in the directory.
 	int numL;
 	int numP;
-	int numT;	
-	
+	int numT;
+
 	int i; // handy dandy re-usable iterator through files
 
-	public OpenLevelSelectController() throws Exception {
+	public OpenLevelSelectController(BuilderMenu Menu) throws Exception {
 
 		// Generate FileCount file and its Scanner
 		java.io.File file = new java.io.File("Levels/FileCount.txt");
@@ -24,5 +26,34 @@ public class OpenLevelSelectController {
 		// Close FileCount
 		input.close();
 
+		// At this point I need to iterate through a loop for each type of file.
+		// This will happen a total of (numL+numP+numT) times.
+
+		// For the builder menu, I only need title and thresholds for each level.
+
+		// I'll start with Lightning level generation.
+		for (i = 1; i <= numL; i++) {
+			
+			file = new java.io.File("Levels/Lightning" + i + ".txt");
+			input = new Scanner(file);
+
+			input.nextInt(); //skip the high score
+			input.nextInt(); //skip the max stars
+			String title = input.nextLine();
+			// WARNING: layout below will possibly break (pass by reference). I
+			// need to put these directly into the level constructor when that
+			// line is made.
+			int[] starThresholds = { input.nextInt(), input.nextInt(), input.nextInt() };
+
+			// TODO create a level and add this data
+			// TODO menu.add(level) (add the level to the menu?)
+			
+			// Close current File
+			input.close();
+		}
+
+		
+		
+		
 	}
 }
--- LetterCraze/src/playerControllers/OpenLevelSelectController.java
+++ LetterCraze/src/playerControllers/OpenLevelSelectController.java
@@ -2,6 +2,8 @@ package playerControllers;
 
 import java.util.Scanner;
 
+import playerFiles.*;
+
 public class OpenLevelSelectController {
 
 	// Store the number of available levels in the directory.
@@ -16,7 +18,7 @@ public class OpenLevelSelectController {
 
 	int i; // handy dandy re-usable iterator through files
 
-	public OpenLevelSelectController() throws Exception {
+	public OpenLevelSelectController(PlayerMenu menu) throws Exception {
 
 		// Generate FileCount file and its Scanner
 		java.io.File file = new java.io.File("Levels/FileCount.txt");
@@ -32,6 +34,32 @@ public class OpenLevelSelectController {
 
 		// Close FileCount
 		input.close();
+
+		// At this point I need to iterate through a loop for each type of file.
+		// This will happen a total of (numL+numP+numT) times.
+
+		// For the player menu, I only need a locked boolean, high score,
+		// corresponding stars, title, and thresholds for each level.
+
+		// I'll start with Lightning level generation.
+		for (i = 1; i <= numL; i++) {
+
+			file = new java.io.File("Levels/Lightning" + i + ".txt");
+			input = new Scanner(file);
+
+			Boolean isLocked = (i <= unlockedL);
+
+			int maxScore = input.nextInt();
+			int maxStars = input.nextInt();
+			String title = input.nextLine();
+			// WARNING: layout below will possibly break (pass by reference). I
+			// need to put these directly into the level constructor when that
+			// line is made.
+			int[] starThresholds = { input.nextInt(), input.nextInt(), input.nextInt() };
+
+			// TODO create a level and add this data
+			// TODO menu.add(level) (add the level to the menu?)
+		}
 
 	}
 }

a52ca070183f5026fe9d219c40eb634a08db0f37
Kevin <Kevin@130.215.218.7>
2016-12-08 12:32:53 -0500

    Added rise and replace methods to PlayerBoard

--- LetterCraze/src/playerFiles/PlayerBoard.java
+++ LetterCraze/src/playerFiles/PlayerBoard.java
@@ -26,5 +26,48 @@ public class PlayerBoard {
 			return false;
 		}
 	}
-
+	
+	//Implements 'gravity' in this board by raising 
+	//	letters to fill empty squares where possible
+	//If no letters remain below a square, the square
+	//	remains empty
+	public boolean rise(){
+		for(int i = 0; i < 6; i++){
+			for(int j = 0; j < 6; j++){
+				if(!this.squares[i][j].hasLetter()){
+					this.squares = findLetter(this.squares, i,j);
+				}
+			}
+		}
+		return true;
+	}
+	
+	//Returns a board with the square corresponding to the given row and column filled
+	//	with the first letter found in the squares below or null if there are not letters
+	//	below this square
+	private PlayerSquare[][] findLetter(PlayerSquare[][] theBoard, int row, int col){
+		try{
+			if(!theBoard[row][col + 1].hasLetter()){
+				theBoard = findLetter(theBoard,row,col+1);
+			}
+			theBoard[row][col].setLetter(theBoard[row][col + 1].removeLetter());
+			return theBoard;
+		}
+		catch (Exception e){
+			theBoard[row][col].setLetter(null);
+			return theBoard;
+		}
+	}
+	
+	//Fills all empty squares with random letters
+	boolean replace(){
+		for(int i = 0; i < 6; i++){
+			for(int j = 0; j < 6; j++){
+				if(!this.squares[i][j].hasLetter()){
+					this.squares[i][j].setLetter(new PlayerLetter());
+				}
+			}
+		}
+		return true;
+	}
 }

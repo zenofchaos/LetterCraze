263a4eeb1997070ac7637386093060bf45d11edd
kcorton <kcorton@wpi.edu>
2016-12-11 14:57:29 -0500

    builder save button controller added

---
+++ LetterCraze/src/builderControllers/BuilderSaveController.java
@@ -0,0 +1,21 @@
+package builderControllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import builderGUI.BuilderEditorGUI;
+
+public class BuilderSaveController implements ActionListener{
+	BuilderEditorGUI editorView;
+	
+	public BuilderSaveController(BuilderEditorGUI editorView) {
+		this.editorView = editorView;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

0cf43c001bc3093cfbac2fc1d93acaa836c46c2b
kcorton <kcorton@wpi.edu>
2016-12-11 14:57:04 -0500

    builder clear button controller added

---
+++ LetterCraze/src/builderControllers/BuilderClearController.java
@@ -0,0 +1,21 @@
+package builderControllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import builderGUI.BuilderEditorGUI;
+
+public class BuilderClearController implements ActionListener{
+BuilderEditorGUI editorView;
+	
+	public BuilderClearController(BuilderEditorGUI editorView) {
+		this.editorView = editorView;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

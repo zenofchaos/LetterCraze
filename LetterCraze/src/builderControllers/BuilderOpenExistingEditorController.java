6d7fb9b1b8a3166afe8b8d3dc5ad0f4be05c7ef9
kcorton <kcorton@wpi.edu>
2016-12-13 18:32:00 -0500

    Controller to open existing level created

--- LetterCraze/src/builderControllers/BuilderOpenExistingEditorController.java
+++ LetterCraze/src/builderControllers/BuilderOpenExistingEditorController.java
@@ -1,5 +1,56 @@
 package builderControllers;
 
-public class BuilderOpenBuilderController {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import builderFiles.BuilderLevel;
+import builderFiles.BuilderMenu;
+import builderGUI.BuilderEditorGUI;
+import builderGUI.BuilderSelectLevelGUI;
+
+public class BuilderOpenExistingEditorController implements MouseListener {
+	
+	BuilderSelectLevelGUI menuView;
+	String levelIdentifier;
+	BuilderMenu menu;
+	
+	public BuilderOpenExistingEditorController(BuilderSelectLevelGUI menuView, String levelIdentifier) {
+		this.menuView = menuView;
+		this.levelIdentifier = levelIdentifier;
+		this.menu = menuView.getMenu();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		BuilderLevel level = menu.getLevel(levelIdentifier);
+		level.initBoard();
+		BuilderEditorGUI window = new BuilderEditorGUI(level);
+		window.openWindow();
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
